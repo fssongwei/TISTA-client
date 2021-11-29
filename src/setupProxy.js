90e489814a41c0c42daae7238267e0c6b8cb1487
Wei Song <davidwsong@tencent.com>
2021-11-29 15:18:59 -0500

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     proxy.createProxyMiddleware("/api", {
-      target: "http://tista-api.wei.ai",
+      target: "http://localhost:5000",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
     })
